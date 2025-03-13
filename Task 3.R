af3808df8b8e2cb5d32e6b85dd48a65152890bb2
IsabelRunnebergerKI <isabel.runneberger@ki.se>
2025-03-13 11:28:50 +0100

    Upload

--- Task 3.R
+++ Task 3.R
@@ -1,3 +1,4 @@
+### Task 3
 # Installing packages
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
@@ -5,7 +6,9 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install(version = "3.20")
 
 BiocManager::install("tidyverse")
+BiocManager::install("data.table")
 
 # Loading packages
 library(BiocManager)
 library(tidyverse)
+library(data.table)
---
+++ Task 5.R
@@ -0,0 +1,17 @@
+### Task 5
+data("CO2")
+
+View(CO2)
+?CO2
+# The CO2 data frame has 84 rows and 5 columns of data from an experiment on the cold tolerance of the grass species Echinochloa crus-galli.
+
+CO2_dt <- as.data.table(CO2)
+mean_Mississippi <- mean(CO2_dt[Type=="Mississippi", uptake])
+mean_Mississippi
+median_Mississippi <- median(CO2_dt[Type=="Mississippi", uptake])
+median_Mississippi
+
+mean_Quebec <- mean(CO2_dt[Type=="Quebec", uptake])
+mean_Quebec
+median_Quebec <- median(CO2_dt[Type=="Quebec", uptake])
+median_Quebec
