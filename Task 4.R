eeaecd1448ec16c7f5c5ae84f96ea37fb871e7ea
IsabelRunnebergerKI <isabel.runneberger@ki.se>
2025-03-14 08:08:07 +0100

    Done

--- Task 4.R
+++ Task 4.R
@@ -1,1 +1,21 @@
 ### Task 4
+#1
+sqrt(10)
+
+#2
+log2(32)
+
+#3
+sum(1:1000)
+
+#4
+sum(seq(2,1000,2))
+
+#5
+#  n(n-1)/2 of pairwise comparisons
+100*(100-1)/2
+
+#6
+# N(N−1)(N−2) as a formula for triples
+100*(100-1)*(100-2)
+
